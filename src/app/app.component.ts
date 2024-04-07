e471bbba6d3f4cdc8c63f71211c88faca878c455
Mark Lai <marklai1998@pm.me>
2024-04-07 15:23:39 -0400

    docs: readme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GoogleMap } from '@angular/google-maps'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, GoogleMap],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'atm-today-web-angular';
+
+  options: google.maps.MapOptions = {
+    center: {lat: 40, lng: -20},
+    zoom: 4
+  };
+
 }
