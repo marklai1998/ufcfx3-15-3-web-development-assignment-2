10ad6d30e0bce73ebaae4a9a2048a11270a0a952
Mark Lai <marklai1998@pm.me>
2024-04-07 20:30:23 -0400

    feat: admin listing

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,14 +1,53 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
 import { MatToolbarModule } from '@angular/material/toolbar'
 import { MatButtonModule } from '@angular/material/button'
 import { MatIconModule } from '@angular/material/icon'
 import { RouterLink } from '@angular/router'
 
+import { Atm, listAtm } from '../services/listAtm'
+import { MatButtonToggleModule } from '@angular/material/button-toggle'
+
+import { FormsModule } from '@angular/forms'
+import { MatTableModule } from '@angular/material/table'
+
 @Component({
   selector: 'app-admin',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule, RouterLink],
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    RouterLink,
+    MatButtonToggleModule,
+    FormsModule,
+    MatTableModule,
+  ],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.css',
 })
-export class AdminComponent {}
+export class AdminComponent implements OnInit {
+  atms: Atm[] = []
+  displayedColumns: string[] = [
+    'id',
+    'lat',
+    'long',
+    'district',
+    'bankName',
+    'type',
+    'address',
+    'serviceHours',
+  ]
+
+  selectedLanguage = 'en'
+
+  async list() {
+    const { latest_record } = await listAtm({
+      language: this.selectedLanguage,
+    })
+    this.atms = latest_record
+  }
+
+  ngOnInit() {
+    void this.list()
+  }
+}
